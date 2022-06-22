104db9490ba11dfb3772dc11a401cfca52d87027
Guagoon Siwakulrangsun <gs.guagoon@gmail.com>
2022-06-22 10:53:42 +0700

    practices_stySheet1

--- components/stySheet.js
+++ components/stySheet.js
@@ -1,32 +1,32 @@
-import { StyleSheet, View, Text,} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const stySheet = () => {
-    return (
-        <View style={style.container}>
-            <Text style={styles.title}>React Native</Text>
-        </View>
-    )
+const Stysheet = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>React Native</Text>
+    </View>
+  )
 }
+
+export default Stysheet
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-    backgroundColor: "#EBCCCC"
-  },
-  title: {
-    marginTop: 16,
-    paddingVertical: 8,
-    borderWidth: 4,
-    borderColor: "#AE7676",
-    borderRadius: 6,
-    backgroundColor: "#FFBCF3",
-    color: "white",
-    textAlign: "center",
-    fontSize: 30,
-    fontWeight: "bold"
-  }
-
-});
-
-export default styles;
+    container: {
+      flex: 1,
+      padding: 24,
+      backgroundColor: "#C9A6B1"
+    },
+    title: {
+      marginTop: 16,
+      paddingVertical: 8,
+      borderWidth: 4,
+      borderColor: "#B48896",
+      borderRadius: 6,
+      backgroundColor: "#F1BEE4",
+      color: "white",
+      textAlign: "center",
+      fontSize: 30,
+      fontWeight: "bold"
+    }
+  });
