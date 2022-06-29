dbab926828fcb8e8d72d14b8e850c514bc9d6a41
Guagoon Siwakulrangsun <gs.guagoon@gmail.com>
2022-06-29 11:02:24 +0700

    commit 29/06/2022

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import User from './components/User'
-
+import Logo from './components/Logo'
 const App = () => {
 
   return (
     <View>
+      <Logo/>
       <User/>
     </View>
   )
