5bdcd36d52dcf722f73540b1a1aa61ceb01a2f58
Guagoon Siwakulrangsun <gs.guagoon@gmail.com>
2022-08-10 09:03:37 +0700

    commit 10/08/2022

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ const App = () => {
       {/* <User/> */}
       {/* <LostOfGreetings/> */}
       {/* <MyCustomTextWith/> */}
-      {/* <Count/> */}
+      <Count/>
       {/* <InputText/> */}
       {/* <UserNameandPassword/> */}
       {/* <UserNamePassword/> */}
@@ -35,7 +35,7 @@ const App = () => {
       {/* <TouchablePractice/> */}
       {/* <DynamicStyle/> */}
       {/* <ModalExample/> */}
-      <ModalPractice/>
+      {/* <ModalPractice/> */}
     </View>
   )
 }
